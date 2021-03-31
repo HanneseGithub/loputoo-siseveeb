36738ee0662cfcd251c20d695c19d738c0b69441
Hannes Juurma <hannes@play.ee>
2021-03-31 17:40:00 +0300

    Add sass compiling and compile all .js files to site.js file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,13 @@
 let mix = require('laravel-mix');
 
 mix.sass('./static/main.scss', './style.css')
-    .browserSync('localhost:8888/naiskoor');
+    .combine('static/js/**/*.js', 'static/site.js', true)
+    .browserSync({
+        proxy: 'http://localhost:8888/naiskoor',
+        files: [
+            'static/main.scss',
+            'static/scss/**/*.scss',
+            'static/site.js',
+            'static/js/**/*.js',
+        ]
+    });
