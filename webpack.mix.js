ac8e912821e7cb8bd422a31b9eb587df8e5e6bd3
Hannes Juurma <hannes@play.ee>
2021-04-08 10:23:56 +0300

    [NS-23] Fix webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,9 +24,7 @@ mix.setPublicPath('static')
     .browserSync({
         proxy: 'http://localhost:8888/naiskoor',
         files: [
-            'static/main.scss',
-            'static/scss/**/*.scss',
-            'static/site.js',
-            'static/js/**/*.js',
+            'src/**/*.svg',
+            'templates/**/*.twig',
         ]
 });
