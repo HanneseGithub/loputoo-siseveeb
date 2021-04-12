9147810e9314e0cfba30b5ba332214947a47b5ae
markkopoljakov <markko.poljakov@khk.ee>
2021-04-12 15:44:30 +0300

    [NS-26] Added custom post type and ACF

--- front-page.php
+++ front-page.php
@@ -1,30 +1,20 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+require('lib/google-calendar.php');
 
-$client = new Google_Client();
-//$client->setAuthConfig(__DIR__ . '/client_secret.json');
-$client->setDeveloperKey('AIzaSyBfDbBJ2etJJBZz-duVxS7kLlTjybcf8Ps');
-$client->setScopes(
-    "https://www.googleapis.com/auth/calendar.events.readonly"
+$args = array(
+    'post_type' => 'teated',
+    'posts_per_page' =>  4,
+    'orderby' => array(
+        'date' => 'DESC'
+    )
 );
- $calendarService = new Google_Service_Calendar($client);
 
- $myCalendarID = "markkopoljakov@gmail.com";
- $events = $calendarService->events
-                          ->listEvents($myCalendarID, array(
-                                'singleEvents' => true,
-                                'timeZone' => 'UTC+3',
-                                'orderBy' => 'startTime',
-                                'timeMin' => date(DATE_RFC3339), 
-                                'maxResults' => 5,
-                                ),
-                          )->getItems();
 $context = Timber::context();
 
 $timber_post     = new Timber\Post();
+$context['teated'] = Timber::get_posts($args);
 $context['post'] = $timber_post;
 $context['calendar_events'] = $events;
-Timber::render( array( 'views/front-page.twig', 'views/page.twig' ), $context );
-
-
+Timber::render(array('views/front-page.twig', 'views/page.twig'), $context);
