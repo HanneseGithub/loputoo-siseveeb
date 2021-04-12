90babdaea8d269182c326a8627103d447ea4a703
Hannes Juurma <hannes@play.ee>
2021-04-12 14:52:50 +0300

    Create lib folder which holds theme related logic to make template files and functions.php cleaner

--- functions.php
+++ functions.php
@@ -73,27 +73,25 @@ class StarterSite extends Timber\Site
 		add_filter('login_headerurl', array($this, 'tyan_login_url'));
 		parent::__construct();
 	}
-	/** This is where you can register custom post types. */
+
+	/** Register custom post types defined in lib/custom-types */
 	public function register_post_types()
 	{
+		require('lib/custom-types.php');
 	}
+
 	/** This is where you can register custom taxonomies. */
 	public function register_taxonomies()
 	{
 	}
 
-	// Enqueue scripts
+	/** Enqueue scripts defined in lib/enqueue-scripts */
 	public function enqueue_my_scripts()
 	{
-		// Use jQuery
-		wp_enqueue_script('jquery');
-
-		// Enqueue our stylesheet and JS file with a jQuery dependency.
-		wp_enqueue_style('my-styles', get_template_directory_uri() . '/static/css/main.css', 1.0);
-		wp_enqueue_script('my-js', get_template_directory_uri() . '/static/js/main.js', array('jquery'), '1.0.0', true);
+		require('lib/enqueue-scripts.php');
 	}
 
-	/* This is where login page logo url is changed */
+	/** Change login page logo url */
 	public function tyan_login_url($url)
 	{
 		return 'https://naiskoor.ee';
@@ -117,68 +115,7 @@ class StarterSite extends Timber\Site
 
 	public function theme_supports()
 	{
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support('automatic-feed-links');
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support('title-tag');
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support('post-thumbnails');
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-			)
-		);
-
-		/*
-		 * Enable support for Post Formats.
-		 *
-		 * See: https://codex.wordpress.org/Post_Formats
-		 */
-		add_theme_support(
-			'post-formats',
-			array(
-				'aside',
-				'image',
-				'video',
-				'quote',
-				'link',
-				'gallery',
-				'audio',
-			)
-		);
-
-		add_theme_support('menus');
-
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'               => 216,
-				'width'                => 676,
-				'flex-height'          => false,
-				'flex-width'           => false,
-				'unlink-homepage-logo' => true,
-			)
-		);
+		require('lib/theme-supports.php');
 	}
 
 	/** This Would return 'foo bar!'.
