b25c4ccf3f0d9efda58ae0fa47ee502e5e0b59e2
markkopoljakov <markko.poljakov@khk.ee>
2021-04-19 12:17:55 +0300

    [NS-28] Using ACF and testing Post Type

--- page-uritused.php
+++ page-uritused.php
@@ -1,5 +1,13 @@
 <<?php
 
-$context = Timber::context();
 
+$üritused_args = array(
+    'post_type' => 'üritused',
+    'posts_per_page' =>  4,
+    'orderby' => array(
+        'date' => 'DESC'
+    )
+);
+$context = Timber::context();
+$context['üritused'] = Timber::get_posts($üritused_args);
 Timber::render(array('views/events.twig', 'views/page.twig'), $context);
