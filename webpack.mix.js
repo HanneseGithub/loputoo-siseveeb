e7594e1f1e1ffc418f6357981d877f9bb4d1b40b
Hannes Juurma <hannes@play.ee>
2021-04-26 10:25:16 +0300

    [NS-34] Add dotenv to local development

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 // webpack.mix.js
 let mix = require('laravel-mix');
+require('dotenv').config();
+
 const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 
 mix.webpackConfig({
@@ -22,7 +24,7 @@ mix.setPublicPath('static')
     .sass('src/scss/main.scss', 'static/css/main.css')
     .combine('src/js/**/*.js', 'static/js/main.js', true)
     .browserSync({
-        proxy: 'http://localhost:8888/naiskoor',
+        proxy: process.env.DEVELOPMENT_URL,
         files: [
             'src/**/*',
             'templates/**/*.twig',
