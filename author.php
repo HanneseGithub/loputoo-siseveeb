013c233df2b2c2eb80cc76a4718b01b6020dbd43
indrek <indrek.runkaur@khk.ee>
2021-05-10 21:52:43 +0300

    [NS-38] My info view has notificaitons, and the forms function as intended

--- author.php
+++ author.php
@@ -11,6 +11,9 @@
 acf_form_head();
 acf_enqueue_uploader();
 
+$context          = Timber::context();
+
+// author-profile-info.twig
 if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['editUserInfo']))
 {
     $userID = $_POST['userID'];
@@ -20,7 +23,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['editUserInfo']))
     $changeEmailTo = esc_attr( $_POST['email']);
     $phoneNumber =  $_POST['phoneNumber'];
     $isInSchool = $_POST['isInSchool'];
-    $personalId = $_POST['personalId'];
     
         if (isset( $changeEmailTo )) {
             // check if user is really updating the value
@@ -28,8 +30,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['editUserInfo']))
                 // check if email is free to use
                 if (email_exists( $changeEmailTo  )){
                     // Email exists, do not update value.
-                    // Maybe output a warning.
-                    echo '<script>alert("See e-mail on juba olemas")</script>';
+                    $context['emailAlreadyExists'] = true;
                 } else {
                     $changeEmailFrom == $changeEmailTo;
                }   
@@ -39,9 +40,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['editUserInfo']))
         $userfield = 'user_' . $userID;
         // Update ACF phone number field for this user
         update_field('field_6093c62793e61', $phoneNumber, $userfield);
-        // Update if the user is still a student
-        update_field('field_6093c5812b67e', $isInSchool, $userfield);
-
+        // Update if the user is is_student_or_graduate
+        update_field('field_6093c54e2b67d', $isInSchool, $userfield);
 
         $user_data = wp_update_user(array( 
             'ID' => $userID, 
@@ -51,14 +51,41 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['editUserInfo']))
         ));
 
     if ( is_wp_error( $user_data ) ) {
-        // There was an error; possibly this user doesn't exist.
-        echo '<script>alert("Error")</script>'; 
-        echo 'Error.';
+        $context['anErrorOccuredOnPersonalInfoUpdating'] = true;
     } else {
-        // Success!
-        echo '<script>alert("User profile updated")</script>'; 
+        $context['userChangedHisPersonalInfo'] = true;
     }
 }
+
+
+// author-organization-info-edit.twig
+function editUserRole($userID){
+    $options = array(
+        'post_id' => 'user_' . $userID,
+        'field_groups' => array('group_607d4cd06f6b2'),
+        'submit_value' => __("Muuda andmeid", 'acf'),
+        'updated_message' => __("Andmed muudetud!", 'acf'),
+        'html_submit_button'  => '<input type="submit" class="stylised-button submit-button" value="%s" />',
+    );
+    acf_form($options);
+}
+// On successful update of author info run this code
+if( isset($_GET['updated']) && $_GET['updated'] == 'true' ) {
+    $context['userOrganisationalInfoWasUpdated'] = true;    
+}
+
+
+// Setting up who can edit user organisational info
+$bookie = current_user_can( 'bookie' );
+$admin = current_user_can( 'administrator' );
+$president = current_user_can( 'president' );
+$conductor = current_user_can('conductor');
+$canEditUser = $president || $conductor || $bookie || $admin;
+
+
+global $wp_query;
+
+// author-profile-edit-password.twig
 if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['editUserPassword'])){
     $oldPassword = $_POST['oldPassword'];
     $newPassword1 = $_POST['newPassword1'];
@@ -77,100 +104,17 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['editUserPassword'])){
             wp_set_auth_cookie($user->ID);
             wp_set_current_user($user->ID);
             do_action('wp_login', $user->user_login, $user);
+            $context['successfulInfoUpdate'] = true;
         }else{
-            echo '<script>alert("the new passwords did not match")</scipt>';
+            $context['passwordsDidNotMatch'] = true;
         }
     } else {
-        echo '<script>alert("Wrong password")</scipt>';
+        $context['badPassword'] = true;
     }
 }
 
-if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['managerEditsUserInfo'])){
-    $userID = $_POST['userID'];
-    $userfield = 'user_' . $userID;
-    $voice =  $_POST['voice'];
-    $userTitle = $_POST['userTitle'];
-    $isBeginner = $_POST['is_beginner'];
-    // Update ACF phone number field for this user
-    update_field('field_607d4a411af9b', $voice, $userfield);
-    // Update users title
-    update_field('field_607d4af81af9c', $userTitle, $userfield);
-    // Update if user is beginner field
-    update_field('field_608fe31c8e06a', $isBeginner, $userfield);
-
-}
-// Form for editing user info in the users view.
-function editUserRole($userID){
-    $options = array(
-        'post_id' => 'user_' . $userID,
-        'field_groups' => array('group_607d4cd06f6b2'),
-        'submit_value' => __("Muuda andmeid", 'acf'),
-        'updated_message' => __("Andmed muudetud!", 'acf'),
-        'html_submit_button'  => '<input type="submit" class="stylised-button submit-button" value="%s" />',
-    );
-    acf_form($options);
-}
-
-function editUserOrganisationalInfo(){
-    $options = array(
-    'post_id' => 'user_' . $userID,
-    'field_groups' => array('group_606c616a0f5af'),
-    'submit_value' => __("Salvesta", 'acf'),
-    'updated_message' => __("Andmed muudetud!", 'acf'),
-    'html_submit_button'  => '<input type="submit" class="user-info__save-button" value="%s" />',
-    );
-    acf_form($options);
-}
-
-// Calculate birthday.
-function calculateBirthday($Personal_ID){
-    $century = 0;
-    $centuryIdentificator = (int) substr($Personal_ID, 0, 1);
-    if ($centuryIdentificator < 3) {
-        $century = 18;
-    } elseif ($centuryIdentificator > 2 && $centuryIdentificator < 5) {
-        $century = 19;
-    } elseif ($centuryIdentificator > 4 && $centuryIdentificator < 7) {
-        $century = 20;
-    }
-    $PersonalIdBirthInfo =  substr($Personal_ID, 1, 6);
-    $birthYear = substr($PersonalIdBirthInfo, 0, 2);
-    $birthMonth = substr($PersonalIdBirthInfo, 2, 2);
-    $birthDay = substr($PersonalIdBirthInfo, 4, 2);
-    $birthDate = $birthDay .'. '. $birthMonth . '. '  . $century . $birthYear;
-    return $birthDate;
-}
-
-function returnUserRoles($userRoles){
-    $currentUserRoles= array();
-    foreach ($userRoles as $role) {
-        $currentUserRoles = translate_user_role($role);
-    }
-    return $currentUserRoles;
-}
 
 
-function currentUserWantsToCangePassword(){
-    if(is_user_logged_in()){
-        return true;
-    }
-}
-function returnCurrentUserProfileLink(){
-	$url = get_author_posts_url( get_current_user_id() );
-		return $url;
-    }
-
-// Setting up who can edit user organisational info
-$bookie = current_user_can( 'bookie' );
-$admin = current_user_can( 'administrator' );
-$president = current_user_can( 'president' );
-$conductor = current_user_can('conductor');
-$canEditUser = $president || $conductor || $bookie || $admin;
-
-
-global $wp_query;
-
-$context          = Timber::context();
 $context['canEditUser'] = $canEditUser;
 $context['posts'] = new Timber\PostQuery();
 if ( isset( $wp_query->query_vars['author'] ) ) {
