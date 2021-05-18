a42f2aa90276643aba70727156b425caf3b7bd94
markkopoljakov <markko.poljakov@khk.ee>
2021-05-18 20:50:34 +0300

    [NS-55] Add music player into single-repetoire view and some style fix in repetoire table

--- src/js/components/repertoire/repertoire.js
+++ src/js/components/repertoire/repertoire.js
@@ -1,16 +1,13 @@
 jQuery(document).ready(function ($) {
 
     $('.repertoire-play-button').click(function (e) {
-        let elm = e.currentTarget;
-        
-        let audio = $('#repertoireAudio');
-        console.log(audio);
-        let source = $('#repertoireAudioSource');
-        source.src = $(elm).data('value');
-        console.log($(elm).data('value'));
-        console.log(source);
-        audio[0].pause();
-        audio[0].load(); //call this to just preload the audio without playing
-        audio[0].play(); //call this to play the song right away
+        var elm = e.currentTarget;
+        var audio = document.getElementById('repertoireAudio');
+      
+        var source = document.getElementById('repertoireAudioSource');
+        source.src = elm.getAttribute('data-value');
+      
+        audio.load();
+        audio.play(); 
     });
 });
